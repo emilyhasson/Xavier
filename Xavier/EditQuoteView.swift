7fc80fa50dc1c213adbeb879cbb8157e73df4bc5
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2023-11-05 01:34:34 -0600

    Make EditQuoteView pretty

--- Xavier/EditQuoteView.swift
+++ Xavier/EditQuoteView.swift
@@ -7,117 +7,49 @@
 
 import SwiftUI
 
+
 struct EditQuoteView: View {
-//    let quote: String
-    @State private var pagenum: String = ""
-    @State private var quote_editable: String
-    @State private var isEditing = false
-    
-    init(quote: String) {
-            self._quote_editable = State(initialValue: quote)
-        }
-    
+    let quote: String
     var body: some View {
         ZStack {
-            Color(red: 19/255, green: 22/255, blue: 28/255)
+            Color.light
                 .edgesIgnoringSafeArea(.all)
             ScrollView {
-                
                 VStack {
                     HStack {
                         Text("Quote")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .bold))
-                            .padding()
+                            .foregroundColor(Color.medium)
+                            .font(.system(size: 24))
+                            .padding(.horizontal)
+                            .padding(.top)
                         Spacer()
-                    } //hstack
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 8) // Creating a RoundedRectangle with rounded corners
-                            .stroke(Color.gray, lineWidth: 1)
-//                        Text(quote)
-//                            .foregroundColor(.white)
-//                            .fontWeight(.bold)
-////                            .padding()
-//                        TextField("Type your quote here", text: $quote_editable)
-//                            .font(.system(size: 24, weight: .bold))
-//                            .padding(.leading)
-                        VStack {
-                                   if isEditing {
-                                       ZStack {
-                                           TextField("Edit text", text: $quote_editable)
-                                               .foregroundColor(.white)
-                                               .padding()
-//                                               .onTapGesture {
-//                                                   UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-//                                               }
-                                           Text(quote_editable)
-                                               .opacity(0)
-                                               .padding(.all, 8)
-                                           }
-                                   } else {
-                                       Text(quote_editable)
-                                           .foregroundColor(.white)
-                                           .fontWeight(.bold)
-                                           .multilineTextAlignment(.center) // Center-align multiline text
-                                           .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                                           .padding()
-                                           .onTapGesture {
-                                               isEditing = true
-                                           }
-                                   }
-                               }
-                    }
-                    Spacer()
+                    }//hstack
+                    TextEditBox(initialText: quote, message: "")
+                        .padding(.horizontal)
                     HStack {
                         Text("Source")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(Color.medium)
+                            .font(.system(size: 24))
                             .padding()
                         Spacer()
-                    } //hstack
-                    RoundedRectangle(cornerRadius: 8)                         .stroke(Color.gray, lineWidth: 1) //dropdown menu
-                        .frame(height:30)
-                        .padding(.bottom)
-                    
+                        DropdownView()
+                    }//hstack
                     HStack {
                         Text("Page Number")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .bold))
-//                            .padding()
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 8)                         .stroke(Color.gray, lineWidth: 1)
-                            TextField("#", text: $pagenum)
-                                .font(.system(size: 24, weight: .bold))
-                                .padding(.leading)
-                        }
-                        Spacer()
-                    } //hstack
-                    Spacer()
-                    HStack {
-                        Text("Notes")
-                            .foregroundColor(.white)
-                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(Color.medium)
+                            .font(.system(size: 24))
                             .padding()
-                        Spacer()
+                        TextEditBox(initialText: "#", message: "")
+                            .padding(.horizontal)
                     }
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 8) // Creating a RoundedRectangle with rounded corners
-                            .stroke(Color.gray, lineWidth: 1) // Applying a white stroke with a line width of 2
-//                            .frame(height: 350)
-                       
-                            
-                        ScrollView {
-                            HStack {
-                                Text("Any notes you add will go here sssssssssssssssssssssssssssssssss s s s s  ss  s   s f f f f f f f f f ffjfffffffffjfjfjffsdndfdkjnfkjsdnfknkjdbsgkkfnvkrjnfawjfkkss \n ssdf \n\nsdad")
-                                    .foregroundColor(.white)
-                                    .padding()
-                                Spacer()
-                            }
-                        } //scrollview
-                    }//zstack
-                } //vstack
-            } //scrollview
-        }
+                    TextEditBox(initialText: "", message: "Add some notes, if you like.")
+                        .padding()
+                }//vstack
+                Text("Done")
+                    .font(.system(size:24, weight: .bold))
+                    .foregroundColor(Color.dark)
+            }//scrollview
+        }//zstack
     }
 }
 
