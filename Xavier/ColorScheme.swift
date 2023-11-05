b255cb3e471b17f3c79d33b7fd43117a24383125
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2023-11-05 01:30:54 -0600

    Add files via upload
    Add color scheme

---
+++ Xavier/ColorScheme.swift
@@ -0,0 +1,15 @@
+//
+//  ColorScheme.swift
+//  Xavier
+//
+//  Created by Ajith Senthil on 11/5/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let light = Color(red: 237/255, green: 221/255, blue: 211/255)
+    static let medium = Color(red: 110/255, green: 81/255, blue: 60/255)
+    static let dark = Color(red: 41/255, green:21/255, blue:8/255)
+}
