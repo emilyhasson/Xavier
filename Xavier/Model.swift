3b802c7bb513ef6b80fd395732ba63d2d5e80212
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2023-11-05 01:38:28 -0600

    Cleaning up

--- Xavier/Model.swift
+++ Xavier/Model.swift
@@ -7,21 +7,6 @@
 
 import SwiftUI
 
-struct Movie: Identifiable {
-    var id: Int
-    var title: String
-    var imageName: String
-}
-
-var latestMovie = [Movie(id: 0, title: "Avengers", imageName: "blue"),
-                   Movie(id: 1, title: "Avengers 2", imageName: "blue"),
-                   Movie(id: 2, title: "Avengers 3", imageName: "blue"),
-                   Movie(id: 3, title: "Avengers 4", imageName: "blue"),
-                   Movie(id: 4, title: "Avengers 5", imageName: "blue"),
-                   Movie(id: 5, title: "Avengers 6", imageName: "blue"),
-                   Movie(id: 6, title: "Avengers 7", imageName: "blue"),
-                   Movie(id: 7, title: "Avengers 8", imageName: "blue"),
-                   Movie(id: 8, title: "Avengers 9", imageName: "blue")]
 
 struct Quote: Identifiable {
     var id: Int
@@ -59,30 +44,56 @@ var quotes = [Quote(id:0, content:
                         Catherine Earnshaw, may you not rest as long as I am living. You said I killed you--haunt me then. The murdered do haunt their murderers. I believe--I know that ghosts have wandered the earth. Be with me always--take any form--drive me mad. Only do not leave me in this abyss, where I cannot find you! Oh, God! It is unutterable! I cannot live without my life! I cannot live without my soul!
                         """,
                     source: 1, pageNum: "104")
+              ,
+        Quote(id:2, content:
+                  """
+                  The truth will set you free. But not until it is finished with you.
+                  """,
+              source: 0, pageNum: "555")
+              ,
+        Quote(id:3, content:
+                  """
+                  I'll say God seems to have a kind of laid-back management style I'm not crazy about. I'm pretty much anti-death. God looks by all accounts to be pro-death. I'm not seeing how we can get together on this issue, he and I...
+                  """,
+              source: 0, pageNum: "876")
+              ,
+        Quote(id:4, content:
+                  """
+                  Do I contradict myself?
+                  Very well then I contradict myself,
+                  (I am large, I contain multitudes.)
+                  """,
+              source: 2, pageNum: "104")
     ]
 
 var sources = [Source(id: 0,
                       title: "Infinite Jest",
 //                      subCollections: [Collection(id: 3, title: "Chapter 1", quotes: [0], imageName: "blue-h")],
-                      quotes: [0, 1],
+                      quotes: [0,2,3],
                       imageName: "sepia-h"),
                Source(id: 1,
                       title: "Wuthering Heights",
-                      imageName: "sepia-h")]
+                      quotes: [1],
+                      imageName: "sepia-h"),
+               Source(id: 2,
+                      title: "Walt Whitman",
+                      quotes: [4],
+                      imageName: "sepia-h")
+]
 
 var collections = [Collection(id: 0,
-                              title: "Inspirational",
-                              quotes: [0, 1],
+                              title: "Poetry",
+                              quotes: [5],
                               imageName: "sepia-v"
                              ),
                    Collection(id: 1,
                               title: "Funny",
 //                              subCollections:
 //                                [Collection(id: 4, title: "IJ", quotes: [0,1], imageName: "blue-h")],
-                              quotes: [0],
+                              quotes: [0,4,3],
                               imageName: "sepia-v"
                              ),
                    Collection(id: 2,
                               title: "Writing",
-                              quotes: [1],
+                              quotes: [1,2],
                               imageName: "sepia-v")]
