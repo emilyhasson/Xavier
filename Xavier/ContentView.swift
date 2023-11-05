fe77e1a2dc74de9d87851d9f94cb42018d2b1926
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2023-11-05 01:32:31 -0600

    Color Scheme Extension

--- Xavier/ContentView.swift
+++ Xavier/ContentView.swift
@@ -7,10 +7,6 @@
 
 import SwiftUI
 
-// light: Color(red: 237/255, green: 221/255, blue: 211/255)
-// med: Color(red: 110/255, green: 81/255, blue: 60/255)
-// dark: Color(red: 41/255, green:21/255, blue:8/255)
-
 struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
@@ -22,13 +18,28 @@ struct ContentView: View {
             ZStack {
                 ScrollView {
                     VStack (spacing: 8) {
-                        Text("Welcome")
-                            .font(.largeTitle)
-                            .foregroundColor(Color(red: 41/255, green:21/255, blue:8/255))
+                        HStack {
+                            Image(systemName: "line.horizontal.3")
+                                .font(Font.title.weight(.bold))
+                                .padding(.leading)
+                                .foregroundColor(Color.medium)
+                            Spacer()
+                            Text("Welcome")
+                                .font(.largeTitle)
+                                .foregroundColor(Color.dark)
+                            Spacer()
+                            Button(action: { showingImagePicker = true }) {
+                                Image(systemName: "plus")
+                                    .font(Font.title.weight(.bold))
+                                    .padding(.trailing)
+                                    .foregroundColor(Color.dark)
+                            } // Button
+                                
+                        } //hstack
                         Carousel(categoryName: "Your Collections")
                         HStack {
                             Text("Sources")
-                                .foregroundColor(Color(red: 41/255, green:21/255, blue:8/255))
+                                .foregroundColor(Color.dark)
                                 .font(.system(size: 24))
                                 .padding(.leading)
                                 .padding(.top)
@@ -38,34 +49,10 @@ struct ContentView: View {
                         ListView()
                     }//vstack
                 }//scrollview
-                .background(Color(red: 237/255, green: 221/255, blue: 211/255))
+                .background(Color.light)
                 .navigationBarHidden(true) // Hide the default navigation bar
                 .navigationBarTitle("") // Set an empty navigation title
                 .navigationViewStyle(StackNavigationViewStyle()) // For full screen appearance
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: { showingImagePicker = true }) {
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .foregroundColor(Color(red: 41/255, green:21/255, blue:8/255))
-                                .background(Color(red: 237/255, green: 221/255, blue: 211/255))
-                                .clipShape(Circle())
-                        } // Button
-                        .padding()
-                    }//hstack
-                    
-//                    ZStack {
-//                        Rectangle()
-//                            .fill(.secondary)
-//                        image?
-//                            .resizable()
-//                             .scaledToFit()
-//                    }.onTapGesture { showingImagePicker = true }
-                    
-                }//vstack
                 .onChange(of: inputImage) { _ in loadImage() }
                 .sheet(isPresented: $showingImagePicker) { ImagePicker(image: $inputImage) }
                 
